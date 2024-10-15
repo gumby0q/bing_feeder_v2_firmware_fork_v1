05b7401fcbc73208e77c4c8eeb737f038e3a4736
anatolii <gumbygm0@gmail.com>
2024-10-15 14:48:13 +0300

    fix minor issues, cleanup

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -58,36 +58,38 @@
 
 
 
-#define FEED_STEP        4 /* defines feed step in mm */
+#define FEED_STEP        4 /* defines feed step in mm - should be one of next: 2, 4, 8, 12, 14, 16 */
 
 
 #define BACKLASH_STEP    1 /* dk if it works with other values than 1 */
-#define SENSOR_COUNTER_MAX        (FEED_STEP + /* backlash compensation error */ BACKLASH_STEP)
-volatile uint8_t sensor_counter = SENSOR_COUNTER_MAX;
+#define SENSOR_COUNTER_MAX        (FEED_STEP + /* increment for backlash compensation error */ BACKLASH_STEP)
+volatile uint8_t sensor_counter = 0;
+volatile uint8_t sensor_counter_falling_edge = 0;
 
 
-#define STATE_IDLE                          1
-#define STATE_MOVE_REEL_FORWARD             2
-#define STATE_MOVE_REEL_BACKWARD            3
-#define STATE_MOVE_REEL_FORWARD_BACKLASH    4
+#define STATE_INITIAL_SET                   1
+#define STATE_IDLE                          2
+#define STATE_MOVE_REEL_FORWARD             3
+#define STATE_MOVE_REEL_BACKWARD            4
+#define STATE_MOVE_REEL_FORWARD_BACKLASH    5
 volatile uint8_t state_machine_status = STATE_IDLE;
 
+#define ACTION_STATUS_MASK_0_SENSOR         0
+#define ACTION_STATUS_MASK_1_BUTTON_0       1
+// #define ACTION_STATUS_MASK_1_BUTTON_0_LONG  2
+#define ACTION_STATUS_MASK_2_COMM_IN        3
+// volatile uint8_t actions_status = 0;
 
-#define BUTTON_STATUS_MASK_0_SENSOR         0
-#define BUTTON_STATUS_MASK_1_BUTTON_0       1
-// #define BUTTON_STATUS_MASK_1_BUTTON_0_LONG  2
-#define BUTTON_STATUS_MASK_2_COMM_IN        3
-// volatile uint8_t buttons_status = 0;
-volatile uint8_t buttons_status = 1 << BUTTON_STATUS_MASK_1_BUTTON_0; /* set defaut value to 1 if you want "pressup" */
-// volatile uint8_t buttons_status_long = 1 << BUTTON_STATUS_MASK_1_BUTTON_0;
-volatile uint8_t buttons_status_long = 0;
+/* set defaut value to 1 (example: 1 << ACTION_STATUS_MASK_1_BUTTON_0) if you want "pressup" with active button state low */
+volatile uint8_t actions_status = 1 << ACTION_STATUS_MASK_1_BUTTON_0;
+volatile uint8_t actions_status_long = 0;
 
-volatile uint8_t buttons_status_flags = 0;
-volatile uint8_t buttons_status_flags_long = 0;
-volatile uint16_t button_debounce_counter_0 = 0;
+volatile uint8_t actions_status_flags = 0;
+volatile uint8_t actions_status_flags_long = 0;
+volatile uint16_t sensor_debounce_counter_0 = 0;
 volatile uint16_t button_debounce_counter_1 = 0;
 volatile uint16_t button_debounce_counter_1_long = 0;
-volatile uint16_t button_debounce_counter_2 = 0;
+volatile uint16_t command_debounce_counter_2 = 0;
 
 #define SENSOR_DEBOUNCE_VALUE  10*1 /* x100 uS */
 #define COMMAND_DEBOUNCE_VALUE  10*4 /* x100 uS */
@@ -104,35 +106,35 @@ void sensors_check(void)
   /* pin1  */
   pin_status = HAL_GPIO_ReadPin(SENSOR_IN_GPIO_Port, SENSOR_IN_Pin) == 0;
 
-  old_pin_status = (buttons_status & (1 << BUTTON_STATUS_MASK_0_SENSOR)) == 0;
+  old_pin_status = (actions_status & (1 << ACTION_STATUS_MASK_0_SENSOR)) == 0;
   if (pin_status != old_pin_status)
   {
-    button_debounce_counter_0 = button_debounce_counter_0 + 1;
+    sensor_debounce_counter_0 = sensor_debounce_counter_0 + 1;
 
-    if (button_debounce_counter_0 >= SENSOR_DEBOUNCE_VALUE)
+    if (sensor_debounce_counter_0 >= SENSOR_DEBOUNCE_VALUE)
     {
-      button_debounce_counter_0 = SENSOR_DEBOUNCE_VALUE;
+      sensor_debounce_counter_0 = SENSOR_DEBOUNCE_VALUE;
 
-      // HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, pin_status);
-      // HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, GPIO_PIN_RESET);
+      actions_status = actions_status ^ (1 << ACTION_STATUS_MASK_0_SENSOR);
 
-      buttons_status = buttons_status ^ (1 << BUTTON_STATUS_MASK_0_SENSOR);
+      /* decrease any edge counter */
+      if (sensor_counter > 0) {
+        sensor_counter = sensor_counter - 1;
+      }
 
-      sensor_counter = sensor_counter - 1;
-
-      // if ((buttons_status & (1 << BUTTON_STATUS_MASK_0_SENSOR)) == 0)
-      // {
-      //   buttons_status_flags = buttons_status_flags | (1 << BUTTON_STATUS_MASK_0_SENSOR);
-
-      //   if (sensor_counter > 0) {
-      //     sensor_counter = sensor_counter - 1;
-      //   }
-      // }
+      if ((actions_status & (1 << ACTION_STATUS_MASK_0_SENSOR)) == 0)
+      { 
+        /* decrease falling edge counter */
+        if (sensor_counter_falling_edge > 0)
+        {
+          sensor_counter_falling_edge = sensor_counter_falling_edge - 1;
+        }
+      }
     }
   }
   else
   {
-    button_debounce_counter_0 = 0;
+    sensor_debounce_counter_0 = 0;
   }
 }
 
@@ -143,29 +145,29 @@ void command_check(void)
 
   /* pin3  */
   pin_status = HAL_GPIO_ReadPin(COMM_IN_GPIO_Port, COMM_IN_Pin) == 0;
-  old_pin_status = (buttons_status & (1 << BUTTON_STATUS_MASK_2_COMM_IN)) == 0;
+  old_pin_status = (actions_status & (1 << ACTION_STATUS_MASK_2_COMM_IN)) == 0;
   if (pin_status != old_pin_status)
   {
-    button_debounce_counter_2 = button_debounce_counter_2 + 1;
+    command_debounce_counter_2 = command_debounce_counter_2 + 1;
 
-    if (button_debounce_counter_2 >= COMMAND_DEBOUNCE_VALUE)
+    if (command_debounce_counter_2 >= COMMAND_DEBOUNCE_VALUE)
     {
-      button_debounce_counter_2 = COMMAND_DEBOUNCE_VALUE;
+      command_debounce_counter_2 = COMMAND_DEBOUNCE_VALUE;
 
-      buttons_status = buttons_status ^ (1 << BUTTON_STATUS_MASK_2_COMM_IN);
+      actions_status = actions_status ^ (1 << ACTION_STATUS_MASK_2_COMM_IN);
       
       // HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
       /* press == 0 */
-      /* pressup > 0 and set default buttons_status to 1*/
-      if ((buttons_status & (1 << BUTTON_STATUS_MASK_2_COMM_IN)) == 0)
+      /* pressup > 0 and set default actions_status to 1*/
+      if ((actions_status & (1 << ACTION_STATUS_MASK_2_COMM_IN)) == 0)
       {
-        buttons_status_flags = buttons_status_flags | (1 << BUTTON_STATUS_MASK_2_COMM_IN);
+        actions_status_flags = actions_status_flags | (1 << ACTION_STATUS_MASK_2_COMM_IN);
       }
     }
   }
   else
   {
-    button_debounce_counter_2 = 0;
+    command_debounce_counter_2 = 0;
   }
 }
 
@@ -173,35 +175,12 @@ void buttons_check(void)
 {
   uint8_t pin_status;
   uint8_t old_pin_status;
-  // uint8_t old_pin_status_long;
 
-  // /* pin1  */
-  // pin_status = HAL_GPIO_ReadPin(SENSOR_IN_GPIO_Port, SENSOR_IN_Pin) == 0;
-  // old_pin_status = (buttons_status & (1 << BUTTON_STATUS_MASK_0_SENSOR)) == 0;
-  // if (pin_status != old_pin_status)
-  // {
-  //   button_debounce_counter_0 = button_debounce_counter_0 + 1;
-
-  //   if (button_debounce_counter_0 >= BUTTON_DEBOUNCE_VALUE)
-  //   {
-  //     buttons_status = buttons_status ^ (1 << BUTTON_STATUS_MASK_0_SENSOR);
-
-  //     if ((buttons_status & (1 << BUTTON_STATUS_MASK_0_SENSOR)) == 0)
-  //     {
-  //       buttons_status_flags = buttons_status_flags | (1 << BUTTON_STATUS_MASK_0_SENSOR);
-  //     }
-  //   }
-  // }
-  // else
-  // {
-  //   button_debounce_counter_0 = 0;
-  // }
-
-  /* pin2  */
+  /* BUTTON_0_Pin  */
   pin_status = HAL_GPIO_ReadPin(BUTTON_0_GPIO_Port, BUTTON_0_Pin) == 0;
 
 
-  old_pin_status = (buttons_status & (1 << BUTTON_STATUS_MASK_1_BUTTON_0)) == 0;
+  old_pin_status = (actions_status & (1 << ACTION_STATUS_MASK_1_BUTTON_0)) == 0;
   if (pin_status != old_pin_status)
   {
     button_debounce_counter_1 = button_debounce_counter_1 + 1;
@@ -210,24 +189,24 @@ void buttons_check(void)
     {
       // button_debounce_counter_1 = BUTTON_DEBOUNCE_VALUE;
 
-      buttons_status = buttons_status ^ (1 << BUTTON_STATUS_MASK_1_BUTTON_0);
+      actions_status = actions_status ^ (1 << ACTION_STATUS_MASK_1_BUTTON_0);
 
       /* pressup */
       // if (pin_status == 0) {
-      // if ((buttons_status & (1 << BUTTON_STATUS_MASK_1_BUTTON_0)) == 1)
-      if ((buttons_status & (1 << BUTTON_STATUS_MASK_1_BUTTON_0)) > 0)
+      // if ((actions_status & (1 << ACTION_STATUS_MASK_1_BUTTON_0)) == 1)
+      if ((actions_status & (1 << ACTION_STATUS_MASK_1_BUTTON_0)) > 0)
       {
 
         /* long press override */
-        // if ((buttons_status_long & (1 << BUTTON_STATUS_MASK_1_BUTTON_0)) == 0)
-        if ((buttons_status_long & (1 << BUTTON_STATUS_MASK_1_BUTTON_0)) > 0)
+        // if ((actions_status_long & (1 << ACTION_STATUS_MASK_1_BUTTON_0)) == 0)
+        if ((actions_status_long & (1 << ACTION_STATUS_MASK_1_BUTTON_0)) > 0)
         {
-          buttons_status_long = buttons_status_long & ~(1 << BUTTON_STATUS_MASK_1_BUTTON_0); /* clear bit */
+          actions_status_long = actions_status_long & ~(1 << ACTION_STATUS_MASK_1_BUTTON_0); /* clear bit */
 
-          buttons_status_flags_long = buttons_status_flags_long | (1 << BUTTON_STATUS_MASK_1_BUTTON_0);
+          actions_status_flags_long = actions_status_flags_long | (1 << ACTION_STATUS_MASK_1_BUTTON_0);
         } else {
           /* regular scenario */
-          buttons_status_flags = buttons_status_flags | (1 << BUTTON_STATUS_MASK_1_BUTTON_0);
+          actions_status_flags = actions_status_flags | (1 << ACTION_STATUS_MASK_1_BUTTON_0);
         }
       }
       // }
@@ -239,10 +218,7 @@ void buttons_check(void)
     button_debounce_counter_1 = 0;
   }
 
-
-  /* LONG pressdown */
-  // old_pin_status_long = (buttons_status_long & (1 << BUTTON_STATUS_MASK_1_BUTTON_0)) == 0;
-  // if (pin_status != old_pin_status_long)
+  /* BUTTON_0_Pin LONG pressdown */
   if (pin_status == 1)
   {
     button_debounce_counter_1_long = button_debounce_counter_1_long + 1;
@@ -251,19 +227,8 @@ void buttons_check(void)
     {
       button_debounce_counter_1_long = BUTTON_DEBOUNCE_LONG_VALUE;
 
-      // buttons_status_long = buttons_status_long ^ (1 << BUTTON_STATUS_MASK_1_BUTTON_0);
-      // buttons_status_long = buttons_status_long & ~(1 << BUTTON_STATUS_MASK_1_BUTTON_0);
-      buttons_status_long = buttons_status_long | (1 << BUTTON_STATUS_MASK_1_BUTTON_0);
-
-      /* press == 0 */
-      /* pressup > 0 */
-      /* see short pressup */
-      // if ((buttons_status_long & (1 << BUTTON_STATUS_MASK_1_BUTTON_0)) == 0)
-      // {
-      //   // buttons_status_flags_long = buttons_status_flags_long & ~(1 << BUTTON_STATUS_MASK_1_BUTTON_0); /* clear bit */
-
-      //   buttons_status_flags_long = buttons_status_flags_long | (1 << BUTTON_STATUS_MASK_1_BUTTON_0);
-      // }
+      /* set long hold status */
+      actions_status_long = actions_status_long | (1 << ACTION_STATUS_MASK_1_BUTTON_0);
     }
   }
 
@@ -271,31 +236,6 @@ void buttons_check(void)
   {
     button_debounce_counter_1_long = 0;
   }
-
-
-  // /* pin3  */
-  // pin_status = HAL_GPIO_ReadPin(COMM_IN_GPIO_Port, COMM_IN_Pin) == 0;
-  // old_pin_status = (buttons_status & (1 << BUTTON_STATUS_MASK_2_COMM_IN)) == 0;
-  // if (pin_status != old_pin_status)
-  // {
-  //   button_debounce_counter_2 = button_debounce_counter_2 + 1;
-
-  //   if (button_debounce_counter_2 >= BUTTON_DEBOUNCE_VALUE)
-  //   {
-  //     buttons_status = buttons_status ^ (1 << BUTTON_STATUS_MASK_2_COMM_IN);
-      
-  //     // HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
-  //     if ((buttons_status & (1 << BUTTON_STATUS_MASK_2_COMM_IN)) == 0)
-  //     {
-  //       buttons_status_flags = buttons_status_flags | (1 << BUTTON_STATUS_MASK_2_COMM_IN);
-  //     }
-  //   }
-  // }
-  // else
-  // {
-  //   button_debounce_counter_2 = 0;
-  // }
-  //   // HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
 }
 
 /* ---------------------------------- */
@@ -393,7 +333,15 @@ int main(void)
   HAL_TIM_Base_Start_IT(&htim1);
 
   HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, GPIO_PIN_SET);
-  Tape_motor_forward();
+
+  /* delay for STATE_INITIAL_SET */
+  state_machine_status = STATE_INITIAL_SET;
+  HAL_Delay(100);
+  /* compensate shift for STATE_INITIAL_SET */
+  Reel_motor_backward();
+
+
+  // Tape_motor_forward();
 
   /* USER CODE END 2 */
 
@@ -405,11 +353,19 @@ int main(void)
     // HAL_Delay(1000);
 
 
-
-    if (state_machine_status == STATE_IDLE) {
-      if ((buttons_status_flags & (1 << BUTTON_STATUS_MASK_1_BUTTON_0)) != 0)
+    if (state_machine_status == STATE_INITIAL_SET) {
+      /* if sensor signal is 1 see: pin_status = HAL_GPIO_ReadPin(SENSOR_IN_GPIO_Port, SENSOR_IN_Pin) == 0; */
+      if ((actions_status & (1 << ACTION_STATUS_MASK_0_SENSOR)) == 0) {
+        Reel_motor_stop();
+        HAL_Delay(100);
+        Tape_motor_forward();
+        state_machine_status = STATE_IDLE;
+        // HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, GPIO_PIN_SET);
+      }
+    } else if (state_machine_status == STATE_IDLE) {
+      if ((actions_status_flags & (1 << ACTION_STATUS_MASK_1_BUTTON_0)) != 0)
       {
-        buttons_status_flags = buttons_status_flags & ~(1 << BUTTON_STATUS_MASK_1_BUTTON_0); /* clear bit */
+        actions_status_flags = actions_status_flags & ~(1 << ACTION_STATUS_MASK_1_BUTTON_0); /* clear bit */
 
         state_machine_status = STATE_MOVE_REEL_FORWARD;
         HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, GPIO_PIN_RESET);
@@ -417,21 +373,21 @@ int main(void)
         Tape_motor_stop();
         Reel_motor_forward();
       }
-      if ((buttons_status_flags_long & (1 << BUTTON_STATUS_MASK_1_BUTTON_0)) != 0)
+      if ((actions_status_flags_long & (1 << ACTION_STATUS_MASK_1_BUTTON_0)) != 0)
       {
-        buttons_status_flags_long = buttons_status_flags_long & ~(1 << BUTTON_STATUS_MASK_1_BUTTON_0); /* clear bit */
+        actions_status_flags_long = actions_status_flags_long & ~(1 << ACTION_STATUS_MASK_1_BUTTON_0); /* clear bit */
 
         state_machine_status = STATE_MOVE_REEL_BACKWARD;
         HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, GPIO_PIN_RESET);
-        // sensor_counter = 1; /* 1 is minimum value = 2mm for now */
-        sensor_counter = 2; /* 2 is minimum value = 2mm for now */
+        /* move backward only by falling edge. that made for right fast aligmend event with backlash acidents */
+        sensor_counter_falling_edge = 1; /* 1 is minimum value = 2mm for now */
         Tape_motor_stop();
         Reel_motor_backward();
       }
       
-      if ((buttons_status_flags & (1 << BUTTON_STATUS_MASK_2_COMM_IN)) != 0)
+      if ((actions_status_flags & (1 << ACTION_STATUS_MASK_2_COMM_IN)) != 0)
       {
-        buttons_status_flags = buttons_status_flags & ~(1 << BUTTON_STATUS_MASK_2_COMM_IN); /* clear bit */
+        actions_status_flags = actions_status_flags & ~(1 << ACTION_STATUS_MASK_2_COMM_IN); /* clear bit */
 
         state_machine_status = STATE_MOVE_REEL_FORWARD;
         HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, GPIO_PIN_RESET);
@@ -448,41 +404,51 @@ int main(void)
       //   state_machine_status = STATE_IDLE;
       //   HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, GPIO_PIN_SET);
       // }
+
       if (sensor_counter == 0) {
-        Reel_motor_stop();
-        HAL_Delay(100);
-        // Tape_motor_forward();
-        sensor_counter = BACKLASH_STEP;
-        Reel_motor_backward();
-        state_machine_status = STATE_MOVE_REEL_FORWARD_BACKLASH;
+        // Reel_motor_stop();
         // HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, GPIO_PIN_SET);
+        // HAL_Delay(100);
+        // // Tape_motor_forward();
+        // sensor_counter = BACKLASH_STEP;
+        // Reel_motor_backward();
+        // state_machine_status = STATE_MOVE_REEL_FORWARD_BACKLASH;
+
+        /* dissable backlash by the 'initial' method */
+        Reel_motor_stop();
+        HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, GPIO_PIN_SET);
+        HAL_Delay(100);
+        Reel_motor_backward();
+        state_machine_status = STATE_INITIAL_SET;
       }
     } else if (state_machine_status == STATE_MOVE_REEL_BACKWARD) {
-      if (sensor_counter == 0) {
+      if (sensor_counter_falling_edge == 0) {
+      // if (sensor_counter == 0) {
         Reel_motor_stop();
         HAL_Delay(100);
         Tape_motor_forward();
         state_machine_status = STATE_IDLE;
         HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, GPIO_PIN_SET);
       }
-    } else if (state_machine_status == STATE_MOVE_REEL_FORWARD_BACKLASH) {
-      if (sensor_counter == 0) {
-        Reel_motor_stop();
-        HAL_Delay(100);
-        Tape_motor_forward();
-        state_machine_status = STATE_IDLE;
-        HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, GPIO_PIN_SET);
-      }
-    }
+    } 
+    // else if (state_machine_status == STATE_MOVE_REEL_FORWARD_BACKLASH) {
+    //   if (sensor_counter == 0) {
+    //     Reel_motor_stop();
+    //     HAL_Delay(100);
+    //     Tape_motor_forward();
+    //     state_machine_status = STATE_IDLE;
+    //     // HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, GPIO_PIN_SET);
+    //   }
+    // }
 
 
     // /* process buttons ------- >>>>> */
-    // if ((buttons_status_flags & (1<<BUTTON_STATUS_MASK_0_SENSOR)) != 0) {
-    //   buttons_status_flags = buttons_status_flags & ~(1<<BUTTON_STATUS_MASK_0_SENSOR); /* clear bit */
+    // if ((actions_status_flags & (1<<ACTION_STATUS_MASK_0_SENSOR)) != 0) {
+    //   actions_status_flags = actions_status_flags & ~(1<<ACTION_STATUS_MASK_0_SENSOR); /* clear bit */
     // }
 
-    // if ((buttons_status_flags & (1<<BUTTON_STATUS_MASK_1_BUTTON_0)) != 0) {
-    //   buttons_status_flags = buttons_status_flags & ~(1<<BUTTON_STATUS_MASK_1_BUTTON_0); /* clear bit */
+    // if ((actions_status_flags & (1<<ACTION_STATUS_MASK_1_BUTTON_0)) != 0) {
+    //   actions_status_flags = actions_status_flags & ~(1<<ACTION_STATUS_MASK_1_BUTTON_0); /* clear bit */
 
     //   // HAL_GPIO_TogglePin(LED_0_GPIO_Port, LED_0_Pin);
     //   Reel_motor_stop();
@@ -505,8 +471,8 @@ int main(void)
     //   // // HAL_Delay(1000);
     //   // Tape_motor_stop();
     // }
-    // if ((buttons_status_flags_long & (1<<BUTTON_STATUS_MASK_1_BUTTON_0)) != 0) {
-    //   buttons_status_flags_long = buttons_status_flags_long & ~(1<<BUTTON_STATUS_MASK_1_BUTTON_0); /* clear bit */
+    // if ((actions_status_flags_long & (1<<ACTION_STATUS_MASK_1_BUTTON_0)) != 0) {
+    //   actions_status_flags_long = actions_status_flags_long & ~(1<<ACTION_STATUS_MASK_1_BUTTON_0); /* clear bit */
 
     //   // HAL_GPIO_TogglePin(LED_0_GPIO_Port, LED_0_Pin);
     //   Reel_motor_stop();
@@ -525,8 +491,8 @@ int main(void)
     //   // // HAL_Delay(1000);
     //   // Tape_motor_stop();
     // }
-    // if ((buttons_status_flags & (1<<BUTTON_STATUS_MASK_2_COMM_IN)) != 0) {
-    //   buttons_status_flags = buttons_status_flags & ~(1<<BUTTON_STATUS_MASK_2_COMM_IN); /* clear bit */
+    // if ((actions_status_flags & (1<<ACTION_STATUS_MASK_2_COMM_IN)) != 0) {
+    //   actions_status_flags = actions_status_flags & ~(1<<ACTION_STATUS_MASK_2_COMM_IN); /* clear bit */
       
     //   // HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
     // }
